9d5bef4b19ad6f8ae74899c2fc82f25de174511e
Tien Le <tienlx@gmail.com>
2012-06-19 02:04:04 +0700

    Unprotect pdf file, and add fallback rendering

--- lib/pdf_to_swf-paperclip-processor.rb
+++ lib/pdf_to_swf-paperclip-processor.rb
@@ -1,9 +1,9 @@
 require "paperclip"
 module Paperclip
     class PdfToSwf < Processor
-    
+
     attr_accessor :file, :params, :format
-    
+
     def initialize file, options = {}, attachment = nil
       super
       @file           = file
@@ -21,15 +21,34 @@ module Paperclip
         parameters << @params
         parameters << ":source"
         parameters << ":dest"
-        
+
         parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
-      
-        success = Paperclip.run("pdf2swf", parameters, :source => "#{File.expand_path(src.path)}",:dest => File.expand_path(dst.path))
+
+        src_path = File.expand_path(src.path)
+        dst_path = File.expand_path(dst.path)
+        success = Paperclip.run("pdf2swf", parameters, :source => src_path, :dest => dst_path)
+
+        # This pdf has been protected,
+        # we need to unprotect it before converting to swf.
+        if success =~ /FATAL.+disallows/i
+          postscript      = Tempfile.new([@basename, '.ps'])
+          unprotected_src = Tempfile.new([@basename, 'unprotected', '.pdf'])
+          ps_params       = ":source :dest"
+
+          Paperclip.run("pdf2ps", ps_params, :source => src_path, :dest => File.expand_path(postscript.path))
+          src_path = File.expand_path(unprotected_src.path)
+          Paperclip.run("ps2pdf", ps_params, :source => File.expand_path(postscript.path), :dest => src_path)
+          success = Paperclip.run("pdf2swf", parameters, :source => src_path, :dest => dst_path)
+        end
       rescue Cocaine::CommandLineError => e
-        raise PaperclipError, "There was an error converting #{@basename} to swf"
+        begin
+          Paperclip.run("pdf2swf", parameters + " -s poly2bitmap", :source => src_path, :dest => dst_path)
+        rescue
+          raise PaperclipError, "There was an error converting #{@basename} to swf"
+        end
       end
       dst
     end
-    
+
   end
 end
